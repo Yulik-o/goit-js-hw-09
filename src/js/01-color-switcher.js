79b1fc9d4d62141abe47bf55f9567db44c01fe9e
Iuliia Goriun <goriunyuliya@gmail.com>
2023-01-10 23:10:04 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,19 +2,28 @@ const buttonStart = document.querySelector('button[data-start]');
 const buttonStop = document.querySelector('button[data-stop]');
 const bodyEl = document.querySelector('body');
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 buttonStart.addEventListener('click', onButtonStartClick);
 buttonStop.addEventListener('click', onButtonStopClick);
 
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
+function onButtonStartClick(event) {
+  
 
-function  onButtonStartClick(event) {
-const randomColor = getRandomHexColor();
-
-bodyEl.style.backgroundColor = randomColor;
+  timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    bodyEl.style.backgroundColor = randomColor;
+  }, 1000);
+  buttonStart.setAttribute("disabled", true);
+  buttonStop.removeAttribute("disabled");
 }
 
-
-
-
+function onButtonStopClick(event) {
+  buttonStart.removeAttribute("disabled");
+  buttonStop.setAttribute("disabled", true);
+  clearInterval(timerId);
+}
